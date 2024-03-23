ae2fd8a3a251af40cbc6735b227201948d1f1179
BianchiDev <ifpebianchi@gmail.com>
2024-03-23 11:02:44 -0300

    Iniciando variável

---
+++ PHP_livro/Variaveis/variavel.php
@@ -0,0 +1,10 @@
+<!-- Variáveis são identificadores utilizados para representar valores mutáveis e voláteis, 
+que só existem durante a execução do programa.  Ela são armazenada na memôria RAM e seu conteúdo e seu conteúdo após 
+a execução do programa. Para criar uma variável em PHP, precisamos atribuir-lhe um nome de identificação, sempre precedido
+pelo caractere cifrão($) -->
+
+<?php
+$nome = "João";
+$Sobremone = "da Silva";
+echo "$Sobremone, $nome";
+?>
