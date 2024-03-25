5d6e78ba15bca8cfcbb484d1b246011097464c3b
BianchiDev <ifpebianchi@gmail.com>
2024-03-25 13:34:35 -0300

    Acrescentando novas  funções a branch fucnções

---
+++ PHP_livro/funcao/strReplace.php
@@ -0,0 +1,5 @@
+<!-- str_replace substitui uma string por outra em um dado contexto. -->
+<?php
+$txt = "O rato roeu a roupa do rei de roma";
+print str_replace(' rato', ' Leão',  $txt);
+?>
---
+++ PHP_livro/funcao/strpos.php
@@ -0,0 +1,14 @@
+<!-- strpos encontra a primeira ocorrência de uma string dentro de outra. -->
+<?php
+$minha_string = 'O rato roeu a roupa do rei de Roma';
+$encotrar = 'roupa';
+$posicao = strpos($minha_string, $encotrar);
+if($posicao)
+{
+    echo "String encontrada na posição $posicao";
+}
+else
+{
+    echo 'String não encontrada';
+}
+?>
